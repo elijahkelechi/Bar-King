8ec3c664abb605aa6580510a5d436ccc521d481f
THANKGOD EIJAH NWANKWO <thankgodnwankwo22@gmail.com>
2024-09-04 14:42:18 +0100

    my

--- src/assets/StyledComponents/CocktailList.js
+++ src/assets/StyledComponents/CocktailList.js
@@ -4,7 +4,6 @@ const CocktailListContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap: 2rem;
-  margin: 50px;
 `;
 
 export default CocktailListContainer;
