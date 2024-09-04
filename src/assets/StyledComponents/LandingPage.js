bb6c83349af16eb8c101b9dab14c4f1447489dbf
THANKGOD EIJAH NWANKWO <thankgodnwankwo22@gmail.com>
2024-09-04 13:42:20 +0100

    third commit

--- src/assets/StyledComponents/LandingPage.js
+++ src/assets/StyledComponents/LandingPage.js
@@ -3,5 +3,6 @@ import styled from "styled-components";
 const LandingPageContainer = styled.div`
   box-sizing: border-box;
   padding: 50px;
+  margin: auto;
 `;
 export default LandingPageContainer;
